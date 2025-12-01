cb90952d6e884743c61cfcb0139699370d6e251d
Kit Chestnut <kitchestnut@hotmail.com>
2025-12-01 07:04:12 -0600

    Fix migration down_revision references

--- backend/alembic/versions/005_add_phase5_studio_customization_tables.py
+++ backend/alembic/versions/005_add_phase5_studio_customization_tables.py
@@ -18,7 +18,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '005_phase5_tables'
-down_revision: Union[str, None] = '004_add_feedback_document_to_beta_requests'
+down_revision: Union[str, None] = 'merge_all_heads_final'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
